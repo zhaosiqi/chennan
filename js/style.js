11a97a799b673dfbc4143d519989c2ee43f82c46
zhaosiqi <1475099074@qq.com>
2016-12-15 08:32:12 +0800

    lilis

---
+++ js/style.js
@@ -0,0 +1,11 @@
+$(function(){
+$(".search3").kuCity();
+ $('video').mediaelementplayer({ 
+        alwaysShowControls: false, 
+        videoVolume: 'horizontal', 
+        features: ['playpause','progress','volume','fullscreen'] 
+    }); 
+})
+//$(document).ready(function() { 
+// 
+//}); 
