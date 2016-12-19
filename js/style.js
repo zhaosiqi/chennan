0866b536b39f785e070a489c040b24f7b34cbae7
zhaosiqi <1475099074@qq.com>
2016-12-19 15:15:53 +0800

    2.0

--- js/style.js
+++ js/style.js
@@ -1,10 +1,5 @@
 $(function(){
 $(".search3").kuCity();
- $('video').mediaelementplayer({ 
-        alwaysShowControls: false, 
-        videoVolume: 'horizontal', 
-        features: ['playpause','progress','volume','fullscreen'] 
-    }); 
 })
 //$(document).ready(function() { 
 // 
